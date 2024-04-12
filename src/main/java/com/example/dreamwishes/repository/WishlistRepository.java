b5190cc71b68a39f642b0a5f0ac46e48e1792c55
Asger <asgervb@gmail.com>
2024-04-12 10:56:16 +0200

    removed methods that was unused in UserRepository and WishlistRepository and now the code can be run. Still need fixing

--- src/main/java/com/example/dreamwishes/repository/WishlistRepository.java
+++ src/main/java/com/example/dreamwishes/repository/WishlistRepository.java
@@ -10,19 +10,5 @@ import java.util.List;
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
     List<Wishlist> findByUserUsernameContaining(String username);
 
-
-    // Find a wishlist by user ID
-    Wishlist findByUserId(Long userId);
-
-    // Find wishlists by user's username
-    List<Wishlist> findByUserUsername(String username);
-
-    // Find wishlists by user's email
-    List<Wishlist> findByUserEmail(String email);
-
-    // Find wishlists by user's name containing a keyword
-    List<Wishlist> findByUserUserNameContaining(String userName);
-
-    // You can define more query methods as needed for your application
 }
 

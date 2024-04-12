d15f9dbffff2c21f36606b5722773c5fb1c8950b
Asger <asgervb@gmail.com>
2024-04-12 18:24:48 +0200

    I was reading on springboots website that they are moving towers componenet based security, so ill try to do that

--- src/main/java/com/example/dreamwishes/SecurityConfig.java
+++ src/main/java/com/example/dreamwishes/SecurityConfig.java
@@ -1,23 +1,33 @@
 package com.example.dreamwishes;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration {
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/", "/home").permitAll() // Allow access to homepage without authentication
-                .anyRequest().authenticated() // Require authentication for all other requests
-                .and()
-                .formLogin() // Enable form-based authentication
-                .loginPage("/login") // Specify the custom login page URL if you have one
-                .permitAll(); // Allow access to the login page without authentication
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .antMatchers("/", "/home").permitAll() // Allow access to homepage without authentication
+                                .anyRequest().authenticated() // Require authentication for all other requests
+                )
+                .formLogin(formLogin ->
+                        formLogin
+                                .loginPage("/login") // Specify the custom login page URL if you have one
+                                .permitAll() // Allow access to the login page without authentication
+                );
     }
 }

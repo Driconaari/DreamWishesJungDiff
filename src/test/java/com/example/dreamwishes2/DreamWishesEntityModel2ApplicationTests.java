534f654a085c236af1cde2986e46c6b229e78329
Asger <asgervb@gmail.com>
2024-04-18 13:04:59 +0200

    the test failed??

--- src/test/java/com/example/dreamwishes2/DreamWishesEntityModel2ApplicationTests.java
+++ src/test/java/com/example/dreamwishes2/DreamWishesEntityModel2ApplicationTests.java
@@ -1,13 +1,9 @@
 package com.example.dreamwishes2;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DreamWishesEntityModel2ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
